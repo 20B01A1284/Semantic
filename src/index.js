0ca6454efe71a589600531e792a31c13849e42f2
Tejaswini <kolleparatejaswini978@gmail.com>
2022-02-11 17:27:46 +0530

    reusability in comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Comment} from './semantic';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Comment } from "./semantic";
 
 ReactDOM.render(
-    <Comment />,
-    document.getElementById("root")
+  <div className="ui cards" style={{paddingTop : "150px",marginLeft : "400px"}}>
+    <Comment name="Elliot Fu" sub="Friends of Veronika" des="Elliot requested permission to view your contact details"/>
+    <Comment name="Jenny Hess" sub="New Member" des={<>Jenny wants to add you to the group <b>best friends</b>
+      </>}/>
+  </div>,
+  document.getElementById("root")
 );
--- src/semantic.js
+++ src/semantic.js
@@ -1,41 +1,29 @@
 import Faker from "faker";
-import "./styles.css";
-function Comment() {
+function Comment(props) {
   return (
-    <div className="ui cards">
-      <div className="ui card">
-        <div className="Details">
-          <img className="ui image" src={Faker.image.people()} />
-          <div className="ui header">Elliot Fu</div>
-          <h3 className="meta">Friends of Veronika</h3>
-          <div className="description">
-            <p>Elliot requested permission to view your contact details</p>
-          </div>
-        </div>
-        <div className="extra content">
-          <div class="ui buttons">
-            <button class="ui green basic button">Approve</button>
-            <button class="ui red basic button">Decline</button>
-          </div>
-        </div>
+    <>
+  <div class="card">
+    <div class="content">
+      <img class="right floated mini ui image" src={Faker.image.people()}/>
+      <div class="header">
+        {props.name}
       </div>
-      <div className="ui card">
-        <div className="Details">
-          <img className="ui image" src={Faker.image.people()} />
-          <div className="ui header">Jenny Hess</div>
-          <h3 className="meta">New Member</h3>
-          <div className="description">
-            <p>Jenny wants to add you to the group <b>best friends</b></p>
-          </div>
-        </div>
-        <div className="extra content">
-          <div class="ui buttons">
-            <button class="ui green basic button">Approve</button>
-            <button class="ui red basic button">Decline</button>
-          </div>
-        </div>
+      <div class="meta">
+       {props.sub}
+      </div>
+      <div class="description">
+        {props.des}
       </div>
     </div>
+    <div class="extra content">
+      <div class="ui two buttons">
+        <div class="ui basic green button">Approve</div>
+        <div class="ui basic red button">Decline</div>
+      </div>
+    </div>
+  </div>
+  
+    </>
   );
 }
 export { Comment };
